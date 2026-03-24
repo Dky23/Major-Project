5156a82f66115f18e067ac0ec9824d8e205a2f30
Daksh Yadav <dakshncsd@gmail.com>
2026-03-24 16:54:23 +0530

    MonitoringDeviceRepository.java

--- src/main/java/com/example/demo/repository/ProjectRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.example.demo.Project;
-
-public interface ProjectRepository extends MongoRepository<Project, String> {}

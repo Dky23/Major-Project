e518e6d175366194dc904ee37c3bff3a9f256239
Daksh Yadav <dakshncsd@gmail.com>
2026-03-24 16:46:20 +0530

    HealthMetricRepository.java

--- src/main/java/com/example/demo/repository/DepartmentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.example.demo.Department;
-
-public interface DepartmentRepository extends MongoRepository<Department, String> {}
